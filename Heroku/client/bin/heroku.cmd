fed5635b59128f267f85fd00944d7ee411937151
xcoreashu <connorashutosh@gmail.com>
2018-01-01 14:55:17 +0530

    changed greeting

--- Heroku/client/bin/heroku.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-set CLI_BINPATH=%~dp0\heroku.cmd
-"%~dp0\node.exe" "%~dp0\heroku.js" %*
--- Heroku/client/bin/heroku.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-const {run} = require('cli-engine')
-const config = {
-  channel: 'stable',
-  version: '6.14.44-8411caa'
-}
-run({config})
